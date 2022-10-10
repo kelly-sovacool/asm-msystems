51dd577519c16783219c23b2a875704fe3b19931
Kelly Sovacool <kelly-sovacool@users.noreply.github.com>
2022-10-10 18:16:22 -0500

    Remove all template_files/

--- clean.sh
+++ clean.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rm -f jabbrv* template.aux template.log template.tex asm.cls msystems.bst template.pdf
-rmdir template_files/mediabag template_files
+rm -rf jabbrv* template.aux template.log template.tex asm.cls msystems.bst template.pdf template_files/
